bf828fd1e5a0911928437542d708a06b59c78bca
tuNovio <alfgomachi@gmail.com>
2023-07-07 12:20:28 -0500

    componente que renderiza la tarea

---
+++ src/components/Todo.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+import SubTareaContainer from "./subtarea/SubTareaContainer"
+/* eslint-disable react/prop-types */
+export default function Todo({ tarea, handleEliminarTarea, handleChangeTarea, subTareas }) {
+    const [editada, setEditada] = useState(false)
+    const [completar, setCompletar] = useState(false)
+    const content = () => {
+        if (editada) {
+            return (
+                <>
+                    <input
+                        type="text"
+                        value={tarea.descripcion}
+                        onChange={(e) => handleChangeTarea({
+                            ...tarea,
+                            descripcion: e.target.value
+                        })}
+                    />
+                    <button onClick={() => setEditada(false)}>Guardar</button>
+                </>
+            )
+        } else {
+            if (!completar) {
+                return (
+                    <>
+                        <input
+                            type="checkbox"
+                            value={completar}
+                            onClick={(e) => setCompletar(e.target.checked)}
+                        />
+                        {tarea.descripcion}
+                        <div className="tarea-options" >
+                            <button onClick={() => setEditada(true)}>Editar</button>
+                            <button onClick={() => handleEliminarTarea(tarea.id)}>Eliminar</button>
+                            <button>Add</button>
+                        </div >
+                    </ >
+                )
+            } else {
+                return (
+                    <div>
+                        <div style={{ textDecoration: 'line-through', color: 'red' }}>
+                            <input type="checkbox" value={completar} onClick={() => setCompletar(!completar)} />
+                            {tarea.descripcion}
+                        </div>
+                        <div className="tarea-options" >
+                            <button onClick={() => handleEliminarTarea(tarea.id)}>Eliminar</button>
+                        </div >
+                    </div >
+                )
+            }
+        }
+    }
+
+    return (
+        <li>
+            <div className="tarea">
+                {content()}
+            </div>
+            <div>
+                <SubTareaContainer
+                    listaSubtareas={subTareas}
+                />
+            </div>
+        </li>
+    )
+}
