27bd830c7c9c8329fab7057fac73247b0bfdf189
tuNovio <alfgomachi@gmail.com>
2023-07-07 12:29:22 -0500

    agregar el componente AddSubTarea

--- src/components/subtarea/SubTareaContainer.jsx
+++ src/components/subtarea/SubTareaContainer.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
 import SubTareaList from "./subTareaList";
-
+import AddSubTarea from "./AddSubTarea";
 export default function SubTareaContainer({ listaSubtareas }) {
     return (
         <>
+            <AddSubTarea />
             <SubTareaList
                 subTareaList={listaSubtareas}
             />
