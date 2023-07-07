780df4d5ef6c231738ffdf2be0ad5702478eabce
tuNovio <alfgomachi@gmail.com>
2023-07-07 12:20:40 -0500

    componente que renderiza las tareas

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import Todo from "./Todo"
+export default function TodoList({ tareas, handleEliminarTarea, handleChangeTarea }) {
+
+    return (
+        <ul className="tareas">
+            {tareas.map(tarea => (
+                <Todo
+                    tarea={tarea}
+                    handleEliminarTarea={handleEliminarTarea}
+                    handleChangeTarea={handleChangeTarea}
+                    subTareas={tarea.subtarea}
+                    key={tarea.id}
+                />
+            ))}
+        </ul>
+    )
+}
