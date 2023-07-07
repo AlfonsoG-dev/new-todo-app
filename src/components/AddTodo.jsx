afd7645f95d69bd271ee74607e307d4f2fec33d1
tuNovio <alfgomachi@gmail.com>
2023-07-07 12:21:16 -0500

    componente para agregar tareas a la lista de tareas

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+import datos from "../store/db"
+
+export default function AddTodo({ addTarea }) {
+    const [descripcion, setDescripcion] = useState('')
+    const [index, setIndex] = useState(datos.length)
+    function handleClickAdd() {
+        const newTarea = {
+            id: index,
+            descripcion: descripcion,
+            completada: false
+        }
+        addTarea(newTarea)
+        setDescripcion("")
+        setIndex(index + 1)
+    }
+    return (
+        <div className="new-tarea">
+            <label htmlFor="descripcion">
+                <input
+                    type="text"
+                    name="descripcion"
+                    id="descripcion"
+                    value={descripcion}
+                    onChange={(e) => setDescripcion(e.target.value)}
+                />
+            </label>
+            <button onClick={handleClickAdd}>Add</button>
+        </div>
+    )
+}
