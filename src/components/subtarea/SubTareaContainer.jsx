7a4be8a90e5ddf712fd67fb381fba37ed1088602
tuNovio <alfgomachi@gmail.com>
2023-07-08 12:29:14 -0500

    agregar controlador de estado para eliminar y añadir subtareas

--- src/components/subtarea/SubTareaContainer.jsx
+++ src/components/subtarea/SubTareaContainer.jsx
@@ -1,18 +1,23 @@
 /* eslint-disable react/prop-types */
 import SubTareaList from "./subTareaList";
 import AddSubTarea from "./AddSubTarea";
+import { useState } from "react";
 export default function SubTareaContainer({ listaSubtareas }) {
+    const [subTareas, setSubTareas] = useState(listaSubtareas)
     function agregarSubTarea(nSubTarea) {
         console.log("TODO: agregarSubTarea")
+    }
+    function eliminarSubTarea(subTareaId) {
+
     }
     return (
         <>
             <AddSubTarea
-                datos={listaSubtareas}
+                datos={subTareas}
                 addSubTarea={agregarSubTarea}
             />
             <SubTareaList
-                subTareaList={listaSubtareas}
+                subTareaList={subTareas}
             />
         </>
     )
