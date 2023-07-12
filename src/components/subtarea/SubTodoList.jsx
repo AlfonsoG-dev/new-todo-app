1c98e33088eade82916a3c964237e7bb12959e57
tuNovio <alfgomachi@gmail.com>
2023-07-12 18:25:13 -0500

    modificación de variables

--- src/components/subtarea/SubTodoList.jsx
+++ src/components/subtarea/SubTodoList.jsx
@@ -1,25 +1,26 @@
 import SubTarea from "./SubTodo";
 
 export default function SubTareaList({ subTareaList, onDeleteSubTarea, onChangeSubTarea, tareaCompleta }) {
-    const content = () => {
-        if (subTareaList) {
-            return (
-                subTareaList.map(element => (
-                    <SubTarea
-                        subtarea={element}
-                        onDeleteSubTarea={onDeleteSubTarea}
-                        onChangeSubTarea={onChangeSubTarea}
-                        tareaCompleta={tareaCompleta}
-                        key={element.id}
-                    />
-                ))
+    let content;
 
-            )
-        }
+    if (subTareaList) {
+        content = (
+            subTareaList.map(element => (
+                <SubTarea
+                    subtarea={element}
+                    onDeleteSubTarea={onDeleteSubTarea}
+                    onChangeSubTarea={onChangeSubTarea}
+                    tareaCompleta={tareaCompleta}
+                    key={element.id}
+                />
+            ))
+
+        )
     }
+
     return (
         <ul>
-            {content()}
+            {content}
         </ul>
     )
 }
