375a87c7069a6d84795acee781bc78519820dde1
tuNovio <alfgomachi@gmail.com>
2023-07-11 22:45:49 -0500

    trabajar con la copia de los datos no mutar los datos

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import './App.css'
 import TodoContainer from './components/TodoContainer'
 import datos from './store/db'
 function App() {
-
+  const listaDatos = datos.slice()
   return (
     <div className='container'>
-      <TodoContainer listaDatos={datos} title={'Prueba'} />
+      <TodoContainer listaDatos={listaDatos} title={'Prueba'} />
       {/* <TodoContainer listaDatos={[]} title={'Estudios'} /> */}
       {/* TODO// panel de creación de todocontainer para crear espacios de trabajo */}
     </div>
