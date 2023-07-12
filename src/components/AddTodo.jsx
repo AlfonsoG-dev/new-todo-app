706d16e606c3711ac8e0e807b1aa446f1ebeabf8
tuNovio <alfgomachi@gmail.com>
2023-07-12 17:50:52 -0500

    cargar los datos de indice para generar id's de las tareas nuevas

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import datos from "../store/db"
 import { AiFillPlusCircle } from "react-icons/ai";
 // eslint-disable-next-line 
 export default function AddTodo({ onAddTarea }) {
     const [descripcion, setDescripcion] = useState('')
-    const [index, setIndex] = useState(datos.length)
+    const [index, setIndex] = useState(0)
+    useEffect(() => {
+        setIndex(datos.length)
+    }, [datos, setIndex])
     function handleOnSubmit(e) {
         e.preventDefault()
         if (descripcion != "") {
