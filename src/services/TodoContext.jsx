59c2949200eb12249c4ee667706531f785fea1ea
tuNovio <alfgomachi@gmail.com>
2023-07-12 23:15:06 -0500

    incluir context como medio de exposición de datos para los componentes

---
+++ src/services/TodoContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useReducer } from "react";
+import { todoReducer } from "../reducer/todoReducer";
+import datos from "../store/db";
+
+const TodoContext = createContext(null)
+const TodoDispatchContext = createContext(null)
+
+export function TodoProvider({ children }) {
+    const [tareas, dispatch] = useReducer(todoReducer, datos)
+
+    return (
+        <TodoContext.Provider value={tareas}>
+            <TodoDispatchContext.Provider value={dispatch}>
+                {children}
+            </TodoDispatchContext.Provider>
+        </TodoContext.Provider>
+    )
+}
+
+export function useTodos() {
+    return useContext(TodoContext)
+}
+
+export function useTodosDispatch() {
+    return useContext(TodoDispatchContext)
+}
