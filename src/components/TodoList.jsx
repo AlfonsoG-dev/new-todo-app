2782dcf66b34e099a4169de75304ac8a6519cf1d
tuNovio <alfgomachi@gmail.com>
2023-07-12 23:16:39 -0500

    modificación para adoptar context como fuente de datos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,12 @@
+import { useTodos } from "../services/TodoContext"
 import Todo from "./Todo"
-export default function TodoList({ tareas, onDeleteTarea, onChangeTarea }) {
+export default function TodoList() {
+    const tareas = useTodos()
     return (
         <ul className="tareas">
             {tareas.map(tarea => (
                 <Todo
                     tarea={tarea}
-                    onDeleteTarea={onDeleteTarea}
-                    onChangeTarea={onChangeTarea}
                     subTareas={tarea.subtarea}
                     key={tarea.id}
                 />
