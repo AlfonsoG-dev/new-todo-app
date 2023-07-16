f8e62d0dc7b6ef14349bf5dd0af457d4baf5710b
tuNovio <alfgomachi@gmail.com>
2023-07-16 16:49:43 -0500

    creación del board canva

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css'
-import TodoContainer from './components/tarea/TodoContainer'
-import datos from './store/db'
+import { TodoBoard } from "./components/BoardCanvan/Board"
 function App() {
   return (
     <div className='container'>
-      <TodoContainer title={'Prueba'} />
-      {/* <TodoContainer listaDatos={[]} title={'Estudios'} /> */}
-      {/* TODO// panel de creación de todocontainer para crear espacios de trabajo */}
+      <TodoBoard />
     </div>
   )
 }
